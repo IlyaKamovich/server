0eba4f482f48374983902e4e34ed7aac562cfec1
Ilya Kamovich <kamovichilya@gmail.com>
2022-05-18 11:39:04 +0300

    new

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import mongoose from 'mongoose';
 const config = require('./config/app');
 
 const app = express();
-config.express(app);
-config.routes(app);
+//config.express(app);
+//config.routes(app);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'WORK' });
+});
 
 const { mongoUri } = config.app;
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 
 const runServer = async () => {
   try {
